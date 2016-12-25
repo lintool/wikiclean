e20b4786048cfb255c51f24cc2fca613d431b852
lintool <jimmylin@uwaterloo.ca>
2016-12-25 18:31:24 -0500

    Refactoring and better arg parsing.

--- src/main/java/org/wikiclean/DumpEnWikiToPlainText.java
+++ src/main/java/org/wikiclean/DumpEnWikiToPlainText.java
@@ -1,47 +1,56 @@
+/**
+ * WikiClean: A Java Wikipedia markup to plain text converter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wikiclean;
 
-import java.io.PrintStream;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
 import org.wikiclean.WikiClean.WikiLanguage;
 
+import java.io.PrintWriter;
+
 public class DumpEnWikiToPlainText {
-  private static final String INPUT_OPTION = "input";
+  private static class Args {
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    String input;
 
-  @SuppressWarnings("static-access")
-  public static void main(String[] args) throws Exception {
-    Options options = new Options();
-    options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("bz2 Wikipedia XML dump file").create(INPUT_OPTION));
+    @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
+    String output;
+  }
 
-    CommandLine cmdline = null;
-    CommandLineParser parser = new GnuParser();
+  public static void main(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
     try {
-      cmdline = parser.parse(options, args);
-    } catch (ParseException exp) {
-      System.err.println("Error parsing command line: " + exp.getMessage());
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
       System.exit(-1);
     }
 
-    if (!cmdline.hasOption(INPUT_OPTION)) {
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.printHelp(DumpEnWikiToPlainText.class.getCanonicalName(), options);
-      System.exit(-1);
-    }
-
-    String path = cmdline.getOptionValue(INPUT_OPTION);
-    PrintStream out = new PrintStream(System.out, true, "UTF-8");
+    PrintWriter writer = new PrintWriter(args.output, "UTF-8");
     WikiClean cleaner = new WikiCleanBuilder()
                               .withLanguage(WikiLanguage.EN).withTitle(false)
                               .withFooter(false).build();
 
-    WikipediaBz2DumpInputStream stream = new WikipediaBz2DumpInputStream(path);
+    WikipediaBz2DumpInputStream stream = new WikipediaBz2DumpInputStream(args.input);
     String page;
     while ((page = stream.readNext()) != null) {
       if ( page.contains("<ns>") && !page.contains("<ns>0</ns>")) {
@@ -53,8 +62,8 @@ public class DumpEnWikiToPlainText {
         continue;
       }
 
-      out.println(cleaner.getTitle(page).replaceAll("\\n+", " ") + "\t" + s);
+      writer.println(cleaner.getTitle(page).replaceAll("\\n+", " ") + "\t" + s);
     }
-    out.close();
+    writer.close();
   }
 }
