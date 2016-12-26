110231bf07f17fcfb5bc7bec32ac943b483ce70c
lintool <jimmylin@uwaterloo.ca>
2016-12-25 19:41:18 -0500

    Muted test case.

--- src/test/java/org/wikiclean/WikiCleanZhTest.java
+++ src/test/java/org/wikiclean/WikiCleanZhTest.java
@@ -1,13 +1,13 @@
 package org.wikiclean;
 
-import static org.junit.Assert.assertFalse;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.wikiclean.WikiClean.WikiLanguage;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
-import org.wikiclean.WikiClean.WikiLanguage;
+import static org.junit.Assert.assertFalse;
 
 public class WikiCleanZhTest {
   @Test
@@ -15,7 +15,7 @@ public class WikiCleanZhTest {
     String raw = FileUtils.readFileToString(new File("src/test/resources/zhwiki-20150423-id13.xml"), "UTF-8");
     WikiClean cleaner = new WikiCleanBuilder().withLanguage(WikiLanguage.ZH).build();
     String content = cleaner.clean(raw);
-    System.out.println(content);
+    //System.out.println(content);
     //Make sure we've removed category links
     assertFalse(content.contains("Category:数学"));
   }
